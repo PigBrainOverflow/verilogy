290d34ebec589e5c073efe97634baa59b1eb3195
Sijie Kong <sijie_kong@ucsb.edu>
2025-04-04 07:24:29 +0000

    Implementing Zip

--- src/ir/dyn_struct_ir.rs
+++ src/ir/dyn_struct_ir.rs
@@ -1,29 +1,33 @@
+use std::rc::Rc;
 use super::super::ast;
-
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
-pub struct Parameter(String);
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum ParameterExpression {
     Constant(i32),
-    ParamRef(Parameter),
-    Add(Box<ParameterExpression>, Box<ParameterExpression>),
-    Sub(Box<ParameterExpression>, Box<ParameterExpression>),
-    Mul(Box<ParameterExpression>, Box<ParameterExpression>),
-    Div(Box<ParameterExpression>, Box<ParameterExpression>),
+    Parameter(String),
+    Add(Rc<ParameterExpression>, Rc<ParameterExpression>),
+    Sub(Rc<ParameterExpression>, Rc<ParameterExpression>),
+    Mul(Rc<ParameterExpression>, Rc<ParameterExpression>),
+    Div(Rc<ParameterExpression>, Rc<ParameterExpression>),
     // we only support the above operations for now
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct BitTensor {
     // Multi-dimensional tensor of bits
-    pub shape: Vec<ParameterExpression>,
+    pub shape: Vec<Rc<ParameterExpression>>,
 }
 
 #[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct View {
     // Polyhedral view of a tensor
-    pub shape: Vec<ParameterExpression>,
+    pub shape: Vec<Rc<ParameterExpression>>,    // range of the original indices
     pub strides: Vec<Vec<i32>>, // including offsets
+    pub tensor: Rc<BitTensor>, // the original tensor
 }
 
+pub struct Zip {
+    // add to the last dimension
+    pub left: Rc<BitTensor>,
+    pub right: Rc<BitTensor>,
+}
